fad6ccc071bc08fc30e3642cbb0cd49c1cde557f
dactyrafficle <40529986+dactyrafficle@users.noreply.github.com>
2022-11-06 11:21:11 -0500

    Delete a.js

--- a.js
+++
@@ -1,6 +0,0 @@
- window.addEventListener('load', function() {
-  let math = document.getElementsByClassName('math');
-  for (let i = 0; i < math.length; i++) {
-   katex.render(math[i].textContent, math[i]);
-  }
- });
